90d5d153b9944addaf3c6a28daf84c564433f1fa
Albert <dony.nyc@gmail.com>
2023-02-10 23:48:58 +0400

    form updated

--- employers/src/components/employees-add-form/employees-add-form.js
+++ employers/src/components/employees-add-form/employees-add-form.js
@@ -1,25 +1,53 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input
-                    type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input
-                    type="number"
-                    className="form-control new-post-label" />
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button
-                    type="submit"
-                    className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    // оптимизация с добавлением в селектор атрибута name
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const { name, salary } = this.state;
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input
+                        type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name='name'
+                        //контролируемый элемент:
+                        value={name} //обязательно записывать атрибуть VALUE
+                        onChange={this.onValueChange} />
+                    <input
+                        type="number"
+                        placeholder="З/П в $?"
+                        className="form-control new-post-label"
+                        name='salary'
+                        //контролируемый элемент:
+                        value={salary} //обязательно записывать атрибуть VALUE
+                        onChange={this.onValueChange} />
+
+                    <button
+                        type="submit"
+                        className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesAddForm;
