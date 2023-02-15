883357d77d6a641c3f2a41954ccfebdaebf2c982
Albert <dony.nyc@gmail.com>
2023-02-15 11:13:17 +0400

    employee add function

--- employers/src/components/app/app.js
+++ employers/src/components/app/app.js
@@ -1,31 +1,58 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../app-search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
-function App() {
+class App extends Component {
 
-    const data = [
-        { name: "John K.", salary: 800, increase: false, id: 1 },
-        { name: "Alex M.", salary: 1200, increase: true, id: 2 },
-        { name: "Ivan I.", salary: 1800, increase: false, id: 3 }
-    ];
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                { name: "John K.", salary: 800, increase: false, id: 1 },
+                { name: "Alex M.", salary: 1200, increase: true, id: 2 },
+                { name: "Ivan I.", salary: 1800, increase: false, id: 3 }
+            ]
+        }
+    }
 
-    return (
-        <div className="app">
-            <AppInfo />
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            return {
+                data: data.filter(elem => elem.id !== id)
+            }
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+            // const index = data.findIndex(elem => elem.id === id);
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            // const newArr = [...before, ...after];
+            // return {
+            //     data: newArr
+            // }
+        })
+    }
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+
+                <EmployeesList data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm />
             </div>
-
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- employers/src/components/employees-list/employees-list.js
+++ employers/src/components/employees-list/employees-list.js
@@ -1,22 +1,27 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete }) => {
 
-    // const elements = data.map(item => {
-    //     const { id, ...itemProps } = item;
-    //     return (
-    //         <EmployeesListItem key={id} {...itemProps} />
-    //     )
-    // })
-
-    // если у элемента не прописан id, то работаем через индекс:
-    const elements = data.map((item, i) => {
+    const elements = data.map(item => {
         const { id, ...itemProps } = item;
         return (
-            <EmployeesListItem key={i} {...itemProps} />
+            <EmployeesListItem key={id}
+                {...itemProps}
+                onDelete={() => onDelete(id)} />
         )
     })
+
+    // если у элемента не прописан id, то работаем через индекс:
+    // const elements = data.map((item, i) => {
+    //     const { id, ...itemProps } = item;
+    //     return (
+    //         <EmployeesListItem
+    //             key={i}
+    //             {...itemProps}
+    //             onDelete={() => onDelete(id)} />
+    //     )
+    // })
 
     return (
         <ul className="app-list list-group">
